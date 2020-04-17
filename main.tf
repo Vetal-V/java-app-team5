20ee2c9e792713f03dfd5e5fb424b95faafe2027
Wewoaw <44336470+EarthNotMars@users.noreply.github.com>
2020-04-17 10:33:27 +0300

    Updated file 17.04
    - need add credentials

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-#Standart tf file for creating 3vms without loadbalancer
-#TO CHANGE
-#NO proper credential
-#disk require no standarT but standarD
-#Vm resource 2 type //_1, _2//
-
 # By VovaPfaifer for Dev-ops team5
 #terrafor code to make Resorce group .. vm
 # Configure the Microsoft Azure Provider
@@ -13,16 +7,16 @@ provider "azurerm" {
     version = "~>2.0"
     features {}
 
-    subscription_id = "xxxxxxxxxxxxxxxxx"
-    client_id       = "xxxxxxxxxxxx"
-    client_secret   = "xxxxxxxxx"
-    tenant_id       = "xxxxx"
+    subscription_id = "xxxxxxxxx" #find it there https://www.terraform.io/docs/providers/azurerm/guides/service_principal_client_secret.html
+    client_id       = "xxxxxxxxxxx"
+    client_secret   = "xxxxxxxxxxxxx"
+    tenant_id       = "bxxxxxxxxxxxxxxxxx"
 }
 
 # Create a resource group if it doesn't exist
 resource "azurerm_resource_group" "myterraformgroup" {
     name     = "myResourceGroup"
-    location = "easteu"
+    location = "eastus"
 
     tags = {
         environment = "Terraform Demo"
@@ -33,7 +27,7 @@ resource "azurerm_resource_group" "myterraformgroup" {
 resource "azurerm_virtual_network" "myterraformnetwork" {
     name                = "myVnet"
     address_space       = ["10.0.0.0/16"]
-    location            = "easteu"
+    location            = "eastus"
     resource_group_name = azurerm_resource_group.myterraformgroup.name #16 line "myterraformgroup"
 
     tags = {
@@ -52,7 +46,7 @@ resource "azurerm_subnet" "myterraformsubnet" {
 # Create public IPs
 resource "azurerm_public_ip" "myterraformpublicip" {
     name                         = "myPublicIP"
-    location                     = "easteu"
+    location                     = "eastus"
     resource_group_name          = azurerm_resource_group.myterraformgroup.name #16 line "myterraformgroup"
     allocation_method            = "Dynamic"
 
@@ -64,7 +58,7 @@ resource "azurerm_public_ip" "myterraformpublicip" {
 # Create Network Security Group and rule
 resource "azurerm_network_security_group" "myterraformnsg" {
     name                = "myNetworkSecurityGroup"
-    location            = "easteu"
+    location            = "eastus"
     resource_group_name = azurerm_resource_group.myterraformgroup.name
     
     security_rule {
@@ -87,7 +81,7 @@ resource "azurerm_network_security_group" "myterraformnsg" {
 # Create network interface
 resource "azurerm_network_interface" "myterraformnic" {
     name                      = "myNIC"
-    location                  = "easteu"
+    location                  = "eastus"
     resource_group_name       = azurerm_resource_group.myterraformgroup.name #16 line "myterraformgroup"
 
     ip_configuration {
@@ -122,7 +116,7 @@ resource "random_id" "randomId" {
 resource "azurerm_storage_account" "mystorageaccount" {
     name                        = "diag${random_id.randomId.hex}"
     resource_group_name         = azurerm_resource_group.myterraformgroup.name #16 line myterraformgroup
-    location                    = "easteu"
+    location                    = "eastus"
     account_tier                = "Standard" #choose
     account_replication_type    = "LRS"
 
@@ -134,33 +128,30 @@ resource "azurerm_storage_account" "mystorageaccount" {
 # Create virtual machine
 resource "azurerm_linux_virtual_machine" "myterraformvm" {
     name                  = "myVM"
-    location              = "easteu"
+    location              = "eastus"
     resource_group_name   = azurerm_resource_group.myterraformgroup.name #16 line myterraformgroup
     network_interface_ids = [azurerm_network_interface.myterraformnic.id] #81 line
-    size                  = "Standard B1s" #choose 8.7$
+    size                  = "Standard_B1s" #choose 8.7$
 
     os_disk {
         name              = "myOsDisk"
         caching           = "ReadWrite"
-        storage_account_type = "Standart_LRS" #choose
+        storage_account_type = "Standard_LRS" #choose
     }
 
     source_image_reference {
         publisher = "Canonical"
         offer     = "UbuntuServer"
-        sku       = "18.04.0-LTS" #newest version 
+        sku       = "18.04-LTS" #newest version 
         version   = "latest"
     }
 
     computer_name  = "myvm"
-    admin_username = "machine_front" #new user name 
-    disable_password_authentication = true #disable pass or  admin_password = "..."
-        
-    admin_ssh_key {
-        username       = "machine_front" #line 149
-        public_key     = file("/home/machine_front/.ssh/authorized_keys") #change .../azureuser/ on your
-    }
-
+    admin_username = "machine_front" #new user name
+    admin_password = "+Bz;=+Y22kkk"
+    disable_password_authentication = false 
+  
+ #no ssh
     boot_diagnostics {
         storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
     }
@@ -168,75 +159,32 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
     tags = {
         environment = "Terraform Demo"
     }
-    }
+}
     # Create virtual machine number2
-resource "azurerm_linux_virtual_machine" "myterraformvm_1" {
-    name                  = "myVM1"
-    location              = "easteu"
+resource "azurerm_linux_virtual_machine" "myterraformvm_2" {
+    name                  = "myVM2"
+    location              = "eastus"
     resource_group_name   = azurerm_resource_group.myterraformgroup.name #16 line myterraformgroup
     network_interface_ids = [azurerm_network_interface.myterraformnic.id] #81 line
-    size                  = "Standard B1s" #choose 8.7$
+    size                  = "Standard_B1s" #choose 8.7$
 
     os_disk {
         name              = "myOsDisk"
         caching           = "ReadWrite"
-        storage_account_type = "Standart_LRS" #choose
+        storage_account_type = "Standard_LRS" #choose
     }
 
     source_image_reference {
         publisher = "Canonical"
         offer     = "UbuntuServer"
-        sku       = "18.04.0-LTS" #newest version 
-        version   = "latest"
-    }
-
-    computer_name  = "myvm1"
-    admin_username = "machine_back_1" #new user name 
-    disable_password_authentication = true #disable pass or  admin_password = "..."
-        
-    admin_ssh_key {
-        username       = "machine_back_1" #line 149
-        public_key     = file("/home/machine_back_1/.ssh/authorized_keys") #change .../azureuser/ on your
-    }
-
-    boot_diagnostics {
-        storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
-    }
-
-    tags = {
-        environment = "Terraform Demo"
-    }
-    }
-    # Create virtual machine number 3
-resource "azurerm_linux_virtual_machine" "myterraformv_2" {
-    name                  = "myVM3"
-    location              = "easteu"
-    resource_group_name   = azurerm_resource_group.myterraformgroup.name #16 line myterraformgroup
-    network_interface_ids = [azurerm_network_interface.myterraformnic.id] #81 line
-    size                  = "Standard B1s" #choose 8.7 $
-
-    os_disk {
-        name              = "myOsDisk"
-        caching           = "ReadWrite"
-        storage_account_type = "Standart_LRS" #choose
-    }
-
-    source_image_reference {
-        publisher = "Canonical"
-        offer     = "UbuntuServer"
-        sku       = "18.04.0-LTS" #newest version 
+        sku       = "18.04-LTS" #newest version 
         version   = "latest"
     }
 
     computer_name  = "myvm2"
-    admin_username = "machine_back_2" #new user name 
-    disable_password_authentication = true #disable pass or  admin_password = "..."
-        
-    admin_ssh_key {
-        username       = "machine_back_2" #line 149
-        public_key     = file("/home/machine_back_2/.ssh/authorized_keys") #change .../azureuser/ on your
-    }
-
+    admin_username = "machine_back_2" #new user name
+    admin_password = "+Bz;=+Y22kkk"    #no ssh
+    disable_password_authentication = false
     boot_diagnostics {
         storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
     }
@@ -244,4 +192,81 @@ resource "azurerm_linux_virtual_machine" "myterraformv_2" {
     tags = {
         environment = "Terraform Demo"
     }
+}
+    # Create virtual machine number 3
+resource "azurerm_linux_virtual_machine" "myterraformvm_3" {
+    name                  = "myVM3"
+    location              = "eastus"
+    resource_group_name   = azurerm_resource_group.myterraformgroup.name #16 line myterraformgroup
+    network_interface_ids = [azurerm_network_interface.myterraformnic.id] #81 line
+    size                  = "Standard_B1s" #choose 8.7 $
+
+    os_disk {
+        name              = "myOsDisk"
+        caching           = "ReadWrite"
+        storage_account_type = "Standard_LRS" #choose
+    }
+
+    source_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "18.04-LTS" #newest version 
+        version   = "latest"
+    }
+
+    computer_name  = "myvm3"
+    admin_username = "+Bz;=+Y22kkk" #new user name 
+    admin_password = "machineback3"
+    disable_password_authentication = false
+#no ssh   
+ 
+    boot_diagnostics {
+        storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
+    }
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+#database not tested
+resource "azurerm_sql_server" "example" {
+  name                         = "mysqlserver"
+  resource_group_name          = azurerm_resource_group.myterraformgroup.name
+  location                     = "West US"
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+
+  tags = {
+    environment = "Terraform Demo"
+  }
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplesa"
+  resource_group_name      = azurerm_resource_group.myterraformgroup.name
+  location                 = azurerm_resource_group.myterraformgroup.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_sql_database" "example" {
+  name                = "mysqldatabase"
+  resource_group_name = azurerm_resource_group.myterraformgroup.name
+  location            = "West US"
+  server_name         = azurerm_sql_server.example.name
+
+  extended_auditing_policy {
+    storage_endpoint                        = azurerm_storage_account.example.primary_blob_endpoint
+    storage_account_access_key              = azurerm_storage_account.example.primary_access_key
+    storage_account_access_key_is_secondary = true
+    retention_in_days                       = 6
+  }
+
+
+
+  tags = {
+    environment = "Terraform Demo"
+  }
 }
